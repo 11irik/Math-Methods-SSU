32cb4be355ab603a369d238cd3da3499a04ab619
Kirill <lukashinkv@gmail.com>
2019-10-07 19:09:43 +0400

    Added task 2

--- src/task2.java
+++ src/task2.java
@@ -9,33 +9,15 @@ public class task2 {
 
         int[] funcs = {1, 8, 27, 64};
 
-        String nodesString = Arrays.toString(nods);
-        System.out.println(nodesString);
+//        String nodesString = Arrays.toString(nods);
+//        System.out.println(nodesString);
 
-        for (int i = 0; i < nods.length; ++i){
-
-            System.out.println((nods[i] + 0.5) * (nods[i] + 0.5) * (nods[i] + 0.5));
-        }
-
-        for (int i = 0; i < nods.length; ++i) {
-            double dot = nods[i] + 0.5;
-            double interpolant = 0;
-
-            for (int k = 0; k < nods.length; ++k) {
-                float factor = 1;
-
-                for (int n = 0; n < k; ++n) {
-                    factor *= (dot - nods[n]) / (nods[k] - nods[n]);
-                }
-
-                for (int n = k + 1; n < nods.length; ++n) {
-                    factor *= (dot - nods[n]) / (nods[k] - nods[n]);
-                }
-
-                interpolant += funcs[k] * factor;
+        for(int i = 0; i < nods.length; ++i) {
+            for (int j = nods.length - 1; j >= 0; j--) {
+                System.out.printf("%-10s", Math.pow(nods[i], j) + " ");
             }
-
-            System.out.println("L(" + dot + ") = " + interpolant);
+            System.out.printf("%-10s", "= " + funcs[i]);
+            System.out.println();
         }
     }
 }
---
+++ src/task4.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class task4 {
+//    public static void table(ArrayList<Double> a){
+//        if (a.size())
+//    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        Double[] nods = {1.0, 2.0, 3.0, 4.0};
+
+        Double[] funcs = {1.0, 8.0, 27.0, 64.0};
+
+        String nodesString = Arrays.toString(nods);
+        System.out.println(nodesString);
+
+//        for (int i = 0; i < nods.length; ++i){
+//
+//            System.out.println((nods[i] + 0.5) * (nods[i] + 0.5) * (nods[i] + 0.5));
+//        }
+
+
+//        List<Double> workList = new ArrayList<Double>();
+//        for (int i = 0; i < nods.length; ++i) {
+//            double dot = nods[i] + 0.5;
+//            double interpolant = 0;
+//
+//            workList = (List<Double>) Arrays.asList(funcs);
+//            for (int k = 0; k < workList.size()-1; ++k) {
+//                workList.set(k, );
+//
+//                interpolant += 0;
+//            }
+//
+//            System.out.println("L(" + dot + ") = " + interpolant);
+//        }
+    }
+}
